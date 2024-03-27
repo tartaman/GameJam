faa2c0992f311c09e0a7c3a126c9b4ada9958f1e
tartaman <70653340+tartaman@users.noreply.github.com>
2024-03-26 20:26:08 -0600

    Ya salta, corre y se cae

---
+++ GameJam/Assets/Scripts/PlayerHookScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHookScript : MonoBehaviour
+{
+    //Como lo tenemos pensado, el gancho estará en el mapa y solo si está dentro del rango del gancho
+    //podrá aderirse a él
+    [SerializeField]
+    GameObject puntoGancho;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ GameJam/Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,164 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+    //Feature flag
+    [Header("Feature flags")]
+    [SerializeField]
+    bool ActiveHeldJump = true;
+    [Header("KeyBinds")]
+    [SerializeField]
+    private KeyCode RunKey; 
+    //Parametros como velocidad etc
+    [Header("Player params")]
+    [SerializeField]
+    float velocidad;
+    //fuerza del salto
+    [SerializeField]
+    int forceJump;
+    //Hice esta para saber si está en el suelo
+    bool isGrounded;
+    bool isRunning;
+    //Cuanto aumenta la velocidad cuando está corriendo
+    [SerializeField]
+    float runMult;
+    float runningVelocity;
+    Rigidbody2D rb;
+    [Header("Jump related")]
+    //Ambos booleanos para manejar el salto
+    bool JumpPressed;
+    bool JumpReleased;
+
+    //PAra hacer que deje de subri segun un limite
+    private bool startTimer = false;
+    //El tiempo que puede saltar
+    [SerializeField]
+    private float JumpTimer;
+    //Timer para saber cuanto tiempo lleva "saltado"
+    [SerializeField]
+    private float timer;
+    //La gravedad para poderlo regresar a la normalidad cuando deje de presionar espacio
+    [SerializeField]
+    private float gravityScale = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        //LA gravedad que le hayamos puesto al rb del jugador
+        rb.gravityScale = gravityScale;
+        timer = JumpTimer;
+        runningVelocity = velocidad * runMult;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        CheckJump();
+        CheckRun();
+    }
+
+    private void CheckRun()
+    {
+        if (Input.GetKeyDown(RunKey))
+        {
+            isRunning = true;
+        }
+        if (Input.GetKeyUp(RunKey))
+        {
+            isRunning = false;
+        }
+    }
+
+    private void Move()
+    {
+        //Horizontal movement -> HzM
+        float HzM = Input.GetAxis("Horizontal");
+        //Nos movemos diferente si esta corriendo o no
+        if (HzM != 0 && isRunning)
+        {
+            transform.Translate(HzM * runningVelocity * Time.deltaTime, 0, 0);
+        }
+        if (HzM != 0 && !isRunning)
+        {
+            transform.Translate(HzM * velocidad * Time.deltaTime, 0, 0);
+        }
+    }
+    //Saltar, checamos si está en el suelo y le da al boton de saltar
+    private void CheckJump()
+    {
+        //Que tanto salta segun cuanto tiempo apreta el espacio
+        if (isGrounded &&  Input.GetButtonDown("Jump") && ActiveHeldJump)
+        { 
+            JumpPressed = true;
+        }
+        if (Input.GetButtonUp("Jump"))
+        {
+            JumpReleased = true;
+        }
+        //Saltar sin mas si no esta activado el salto feature
+        if (isGrounded && Input.GetButtonDown("Jump") && !ActiveHeldJump)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, forceJump, 0);
+        }
+        //Manejo del salto feature
+        if (JumpPressed)
+        {
+            StartJump();
+        }
+        if (JumpReleased) 
+        {
+            StopJump();
+        }
+        CheckTimer();
+    }
+    //Saber cuando solto el boton de saltar y empezar a bajar
+    private void StopJump()
+    {
+        rb.gravityScale = gravityScale;
+        JumpReleased = false;
+        timer = JumpTimer;
+        startTimer = false;
+    }
+    //Para que sepamos cuando empezó a saltar desde que presiono el boton de saltar
+    private void StartJump()
+    {
+        //para que siga subiendo cuando saltó
+        rb.gravityScale = 0;
+        rb.AddForce(new Vector2(0, forceJump), ForceMode2D.Impulse);
+        JumpPressed = false;
+        startTimer = true;
+    }
+    private void CheckTimer()
+    {
+        if (startTimer)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                JumpReleased = true;
+            }
+        }
+    }
+    //Checar la tag a ver si esta en el piso o en algo en lo que pueda saltar
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Floor"))
+        {
+            isGrounded = true;
+        }
+    }
+    //Checar si sale de una colision con el piso entonces ya no está en el piso
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Floor"))
+        {
+            isGrounded = false;
+        }
+    }
+}
