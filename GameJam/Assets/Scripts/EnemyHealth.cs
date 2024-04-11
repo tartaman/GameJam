68acaebe8f1d206e55ec9aed03ad41817b350efb
tartaman <70653340+tartaman@users.noreply.github.com>
2024-04-10 20:15:00 -0600

    ya tinene vida los enemigos

--- GameJam/Assets/Scripts/EnemyHealth.cs
+++ GameJam/Assets/Scripts/EnemyHealth.cs
@@ -9,16 +9,9 @@ public class EnemyHealth : MonoBehaviour, IDamageable
     [SerializeField] private float currentHealth;
     [SerializeField] FloatingHealthbar healthbar;
 
-    [Header("Enemy Animator")]
-    [SerializeField] public Animator animator;
-
-    [Header("Enemy Things")]
-    [SerializeField] public Rigidbody2D rb;
-
     public void Damage(float damageAmount)
     {
         //animator.SetTrigger("Hit");
-        rb.velocity = Vector3.zero;
         currentHealth -= damageAmount;
 
         healthbar.UpdateHealthBar(currentHealth, maxHealth);
@@ -31,10 +24,8 @@ public class EnemyHealth : MonoBehaviour, IDamageable
 
     public void Die()
     {
-        rb.velocity = Vector3.zero;
         //animator.SetBool("Death", true);
-
-        Destroy(gameObject,1.5f);
+        Destroy(gameObject);
     }
 
     // Start is called before the first frame update
