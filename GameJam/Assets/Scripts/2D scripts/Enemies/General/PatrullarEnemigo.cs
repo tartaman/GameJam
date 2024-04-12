ca0413c0f712c52677737572033a3cc27d67ba16
Ventthos <117321816+Ventthos@users.noreply.github.com>
2024-04-12 12:37:45 -0600

    Main menu y volver al menu al terminar

--- GameJam/Assets/Scripts/2D scripts/Enemies/General/PatrullarEnemigo.cs
+++ GameJam/Assets/Scripts/2D scripts/Enemies/General/PatrullarEnemigo.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using UnityEngine;
-using static UnityEditor.Experimental.GraphView.GraphView;
-
 public class PatrullarEnemigo : MonoBehaviour
 {
     // Como tal son los puntos máximos a los que puede llegar
