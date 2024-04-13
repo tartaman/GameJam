9da1db95e1e311d9836dfc6ddf7b2d1b34e5b6c4
PlayerAdjustments <ferzg2004@gmail.com>
2024-04-12 18:51:36 -0600

    Finished connections

--- GameJam/Assets/Scripts/2.5D scripts/TPFD_Playground.cs
+++ GameJam/Assets/Scripts/2.5D scripts/TPFD_Playground.cs
@@ -5,13 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class TPFD_Playground : MonoBehaviour
 {
-    [Header("Keybinds")]
-    public KeyCode goToScene = KeyCode.S;
-    public KeyCode goToMainMap = KeyCode.R;
-    public KeyCode TPMain = KeyCode.P;
-    public KeyCode TPForest = KeyCode.F;
-    public KeyCode TPWater = KeyCode.W;
-    public KeyCode TPMine = KeyCode.M;
 
     [Header("Spawnpoints")]
     public GameObject MainMapSpawnpoint;
@@ -27,6 +20,7 @@ public class TPFD_Playground : MonoBehaviour
     public GameObject Canvas;
     private Animator canvasAnim;
     public float transitionTime;
+    public GameObject JoinWorldPanel;
 
 
     // Start is called before the first frame update
@@ -38,8 +32,6 @@ public class TPFD_Playground : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        LoadScene();
-
         if(SceneManager.GetActiveScene().name.Contains("2.5D Map"))
         {
             MainMapSpawnpoint = GameObject.Find("MainSpawnpoint");
@@ -55,58 +47,6 @@ public class TPFD_Playground : MonoBehaviour
         {
             Canvas = GameObject.Find("Canvas");
             canvasAnim = Canvas.GetComponent<Animator>();
-        }
-
-        ManageTPs();
-
-    }
-
-    void LoadScene()
-    {
-
-        if(Input.GetKeyDown(goToScene))
-        {
-            if(SceneManager.GetActiveScene().buildIndex != SceneManager.GetSceneByName(goToSceneName).buildIndex)
-            {
-                StartCoroutine(LoadLevel(goToSceneName));
-
-            }
-
-        }
-
-        if (Input.GetKeyDown(goToMainMap))
-        {
-            Debug.Log($"Load Main map 1 {SceneManager.GetActiveScene().buildIndex} != {SceneManager.GetSceneByName(goToMainMapName).buildIndex}");
-
-            if (SceneManager.GetActiveScene().buildIndex != SceneManager.GetSceneByName(goToMainMapName).buildIndex)
-            {
-                
-                StartCoroutine(LoadLevel(goToMainMapName));
-            }
-        }
-    }
-
-    void ManageTPs()
-    {
-        if (Input.GetKeyDown(TPForest))
-        {
-            StartCoroutine(LoadTP(ForestSpawnpoint.transform));
-            
-        }
-
-        if (Input.GetKeyDown(TPMain))
-        {
-            StartCoroutine(LoadTP(MainMapSpawnpoint.transform));
-        }
-
-        if(Input.GetKeyDown(TPWater))
-        {
-            StartCoroutine(LoadTP(WaterSpawnpoint.transform));
-        }
-
-        if (Input.GetKeyDown(TPMine))
-        {
-            StartCoroutine(LoadTP(MineSpawnpoint.transform));
         }
     }
 
@@ -127,5 +67,7 @@ public class TPFD_Playground : MonoBehaviour
         yield return new WaitForSeconds(transitionTime);
 
         this.transform.position = t.position;
+
+        JoinWorldPanel.SetActive(false);
     }
 }
--- GameJam/Assets/Scripts/2D scripts/NextScene.cs
+++ GameJam/Assets/Scripts/2D scripts/NextScene.cs
@@ -5,8 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class NextScene : MonoBehaviour
 {
+    [Header("GameManager Persistence")]
+    public GameObject gameManager;
+
+    private void Awake()
+    {
+        gameManager = GameObject.Find("GameManager");
+    }
+
     public void NewScene(string name)
     {
+        gameManager.GetComponent<TPFD_GameManager>().isMineLevel1Completed = true;
         SceneManager.LoadScene(name);
     }
 }
